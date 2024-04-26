955f3f3e64a8b4228385a56772bb92214825915e
AzizBenDhiab <mohamedaziz.bendhiab@insat.ucar.tn>
2024-04-26 23:44:14 +0100

    add update and delete cv

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,20 @@ import { createServer } from "node:http";
 import { db } from "./db/db";
 import fs from "fs";
 import { Query } from "./resolvers/Query";
-
+import { Mutation } from "./resolvers/Mutation";
 const yoga = createYoga({
-    schema: createSchema({
-      typeDefs:  fs.readFileSync(
-        "src/schema/schema.graphql",
-        "utf-8"
-      ),
-      resolvers: {
-        Query
-      },
-    }),
+  schema: createSchema({
+    typeDefs: fs.readFileSync("src/schema/schema.graphql", "utf-8"),
+    resolvers: {
+      Query,
+      Mutation,
+    },
+  }),
 
-    context: { db },
-
+  context: { db },
 });
 
 const server = createServer(yoga);
 server.listen(4000, () => {
-    console.info("Server is running on http://localhost:4000/graphql");
-  });
+  console.info("Server is running on http://localhost:4000/graphql");
+});
