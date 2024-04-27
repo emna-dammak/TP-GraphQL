5059435658dd96e9c2976cb06c2b2a08e658e266
AzizBenDhiab <mohamedaziz.bendhiab@insat.ucar.tn>
2024-04-27 08:43:34 +0100

    fix bugs

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -28,68 +28,67 @@ export const Mutation = {
     return cv;
   },
 
-      updateCv: (
-          parent: any,
-          { id, cvUpdateInput }: any,
-          { db }: any,
-          infos: any
-      ) => {
-        console.log(id, cvUpdateInput);
-        if (!existInArray(db.cvs, "id", id)) {
-          throw new GraphQLError(This ID ${id} does not correspond to any cv.);
-        }
+  updateCv: (
+    parent: any,
+    { id, cvUpdateInput }: any,
+    { db }: any,
+    infos: any
+  ) => {
+    console.log(id, cvUpdateInput);
+    if (!existInArray(db.cvs, "id", id)) {
+      throw new GraphQLError(`This ID ${id} does not correspond to any cv.`);
+    }
 
-        if (
-            cvUpdateInput.userId &&
-            !existInArray(db.users, "id", cvUpdateInput.userId)
-        ) {
+    if (
+      cvUpdateInput.userId &&
+      !existInArray(db.users, "id", cvUpdateInput.userId)
+    ) {
+      throw new GraphQLError(
+        `  This ID ${cvUpdateInput.userId} does not correspond to any user.`
+      );
+    }
+
+    if (cvUpdateInput.skillIds) {
+      for (let i = 0; i < cvUpdateInput.skillIds.length; i++) {
+        if (!existInArray(db.skills, "id", cvUpdateInput.skillIds[i])) {
           throw new GraphQLError(
-              This ID ${cvUpdateInput.userId} does not correspond to any user.
-        );
-        }
-
-        if (cvUpdateInput.skillIds) {
-          for (let i = 0; i < cvUpdateInput.skillIds.length; i++) {
-            if (!existInArray(db.skills, "id", cvUpdateInput.skillIds[i])) {
-              throw new GraphQLError(
-                  This ID ${cvUpdateInput.skillIds[i]} does not correspond to any skill.
-            );
-            }
-          }
-
-          const cvSkillsToUpdate = cvUpdateInput.skillIds.map(
-              (skillId: string) => ({
-                id_cv: id,
-                id_skill: skillId,
-              })
+            `This ID ${cvUpdateInput.skillIds[i]} does not correspond to any skill.`
           );
-
-          db.cv_skill = db.cv_skill.filter(
-              (cvSkill: any) =>
-                  cvSkill.id_cv !== id ||
-                  cvUpdateInput.skillIds.includes(cvSkill.id_skill)
-          );
-
-          cvSkillsToUpdate.forEach((cvSkill: any) => {
-            if (
-                !db.cv_skill.some((existingCvSkill: any) =>
-                    compareCvSkills(existingCvSkill, cvSkill)
-                )
-            ) {
-              db.cv_skill.push(cvSkill);
-              console.log(Added cv_skill:, cvSkill);
-            }
-          });
         }
+      }
 
-        const cv = db.cvs.find((cv: any) => cv.id === id);
-        for (let key in cvUpdateInput) {
-          cv[key] = cvUpdateInput[key];
+      const cvSkillsToUpdate = cvUpdateInput.skillIds.map(
+        (skillId: string) => ({
+          id_cv: id,
+          id_skill: skillId,
+        })
+      );
+
+      db.cv_skill = db.cv_skill.filter(
+        (cvSkill: any) =>
+          cvSkill.id_cv !== id ||
+          cvUpdateInput.skillIds.includes(cvSkill.id_skill)
+      );
+
+      cvSkillsToUpdate.forEach((cvSkill: any) => {
+        if (
+          !db.cv_skill.some((existingCvSkill: any) =>
+            compareCvSkills(existingCvSkill, cvSkill)
+          )
+        ) {
+          db.cv_skill.push(cvSkill);
         }
-        cv.user = db.users.find((user: any) => cvUpdateInput.owner == user.id);
+      });
+    }
 
-        return cv;
-      },
+    const cv = db.cvs.find((cv: any) => cv.id === id);
+    for (let key in cvUpdateInput) {
+      cv[key] = cvUpdateInput[key];
+    }
+    cv.user = db.users.find((user: any) => cvUpdateInput.owner == user.id);
+
+    return cv;
+  },
   deleteCv: (parent: any, { id }: any, { db }: any, info: any) => {
     const indexCv = db.cvs.findIndex((cv: any) => cv.id === id);
     if (indexCv === -1) {
@@ -104,6 +103,6 @@ function existInArray(array: any[], attribut: string, value: string): boolean {
   return array.some((element) => element[attribut] == value);
 }
 
-function compareCvSkills(skill1: any, skill2: any, i: any): boolean {
-  return skill1.id_cv === skill2.id && skill1.id_skill === skill2.skillIds[i];
+function compareCvSkills(skill1: any, skill2: any): boolean {
+  return skill1.id_cv === skill2.id_cv && skill1.id_skill === skill2.id_skill;
 }
